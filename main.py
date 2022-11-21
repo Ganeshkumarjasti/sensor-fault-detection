32419dedaf98496011a0c5c0fb4b995ad0a32512
Ganesh <ganeshkumarjasti19@gmail.com>
2022-11-21 17:36:59 +0530

    updated

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from sensor.exception import SensorException
 import os,sys
 from sensor.logger import logging
 from sensor.pipeline import training_pipeline
+import yaml
 from sensor.pipeline.training_pipeline import TrainPipeline
 
-
-
-
 if __name__ == '__main__':
-    training_pipeline = TrainPipeline()
-    training_pipeline.run_pipeline()
+        training_pipeline = TrainPipeline()
+        training_pipeline.run_pipeline()
     
+        
